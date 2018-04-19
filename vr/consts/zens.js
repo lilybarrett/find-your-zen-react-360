d643488ff50f1dde68c1d96d7b9562add2aeba7c
Lily Barrett <lily.barrett@ef.com>
2018-04-18 23:28:12 -0400

    WIP add sounds

--- vr/consts/zens.js
+++ vr/consts/zens.js
@@ -1,15 +1,15 @@
 const zens = [
     {   mantra: "Find your inner motherforking peace",
-        image: "hawaii_beach.jpg"
+        image: "images/hawaii_beach.jpg"
     },
     {   mantra: "Breathe in peace, breathe out bullshirt",
-        image: "horseshoe_bend.jpg"
+        image: "images/horseshoe_bend.jpg"
     },
     {   mantra: "Benches will be benches",
-        image: "sunrise_paris_2.jpg"
+        image: "images/sunrise_paris_2.jpg"
     },
     {   mantra: "",
-        image: "homebase.png"
+        image: "images/homebase.png"
     }
 ]
 

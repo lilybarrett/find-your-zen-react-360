45b34b41bb719e78cc18c0007c6a584631de0f79
Lily Barrett <lily.barrett@ef.com>
2018-04-18 22:58:19 -0400

    Make menu and text disappear on zen selections

--- index.vr.js
+++ index.vr.js
@@ -30,28 +30,32 @@ export default class MeditationApp extends React.Component {
   render() {
     return (
       <View>
-        <View>
-          <Pano source={asset(zens[this.state.selectedZen].image)}/>
-          <Text
-              style={{
-                backgroundColor: '#29ECCE',
-                fontSize: 0.2,
-                fontWeight: '400',
-                layoutOrigin: [0.5, 0.5],
-                paddingLeft: 0.2,
-                paddingRight: 0.2,
-                textAlign: 'center',
-                textAlignVertical: 'center',
-                transform: [{translate: [0, 0, -3]}],
-            }}>
-            Choose your zen
-          </Text>
-        </View>
-        <View>
-          <Button buttonClick={() => this.zenClicked(0)} text="I'm feeling beachy keen" />
-          <Button buttonClick={() => this.zenClicked(1)} text="Ain't no mountain high enough" />
-          <Button buttonClick={() => this.zenClicked(2)} text="I want a baguette" />
-        </View>
+         <Pano source={asset(zens[this.state.selectedZen].image)}/>
+         { this.state.selectedZen !== 3 ? null :
+          <View>
+            <View>
+              <Text
+                  style={{
+                    backgroundColor: '#29ECCE',
+                    fontSize: 0.2,
+                    fontWeight: '400',
+                    layoutOrigin: [0.5, 0.5],
+                    paddingLeft: 0.2,
+                    paddingRight: 0.2,
+                    textAlign: 'center',
+                    textAlignVertical: 'center',
+                    transform: [{translate: [0, 0, -3]}],
+                }}>
+                Choose your zen
+              </Text>
+            </View>
+            <View>
+              <Button buttonClick={() => this.zenClicked(0)} text="I'm feeling beachy keen" />
+              <Button buttonClick={() => this.zenClicked(1)} text="Ain't no mountain high enough" />
+              <Button buttonClick={() => this.zenClicked(2)} text="I want a baguette" />
+            </View>
+          </View>
+         }
       </View>
     );
   }

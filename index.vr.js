0ccd72a7942c4d39dfd4c5c272ef54cb86a1b409
Lily Barrett <lily.barrett@ef.com>
2018-04-18 21:56:27 -0400

    WIP button feature

--- index.vr.js
+++ index.vr.js
@@ -3,82 +3,56 @@ import {
   AppRegistry,
   asset,
   Pano,
+  VrButton,
   Text,
   View,
-  VrButton,
   Image,
 } from 'react-vr';
+import zens from "./vr/consts/zens.js";
+import Button from "./vr/components/button.js";
 
 export default class MeditationApp extends React.Component {
+ constructor () {
+   super();
+   this.state = {
+    selectedZen: 3,
+   }
+
+   this.zenClicked = this.zenClicked.bind(this);
+ }
+
+  zenClicked(zen) {
+    let newZen = zen;
+    console.log(newZen);
+    debugger;
+    this.setState({ selectedZen: newZen });
+  }
+
   render() {
     return (
       <View>
-        <Pano source={asset('homebase.png')}/>
-        <Text
-            style={{
-              backgroundColor: '#29ECCE',
-              fontSize: 0.2,
-              fontWeight: '400',
-              layoutOrigin: [0.5, 0.5],
-              paddingLeft: 0.2,
-              paddingRight: 0.2,
-              textAlign: 'center',
-              textAlignVertical: 'center',
-              transform: [{translate: [0, 0, -3]}],
-          }}>
-          Choose your zen
-        </Text>
-        <VrButton
-          style={{width: 1.0}}
-          onClick={()=> console.log("I was clicked!")}>
+        <View>
+          <Pano source={asset(zens[this.state.selectedZen].image)}/>
           <Text
-            style={{
-              backgroundColor: '#29ECCE',
-              fontSize: 0.07,
-              marginTop: 0.05,
-              layoutOrigin: [0.5, 0.5],
-              fontWeight: '400',
-              textAlign: 'center',
-              textAlignVertical: 'center',
-              transform: [{translate: [0, 0, -3]}],
-          }}>
-            I'm feeling beachy
+              style={{
+                backgroundColor: '#29ECCE',
+                fontSize: 0.2,
+                fontWeight: '400',
+                layoutOrigin: [0.5, 0.5],
+                paddingLeft: 0.2,
+                paddingRight: 0.2,
+                textAlign: 'center',
+                textAlignVertical: 'center',
+                transform: [{translate: [0, 0, -3]}],
+            }}>
+            Choose your zen
           </Text>
-        </VrButton>
-        <VrButton
-          style={{width: 1.0}}
-          onClick={()=> console.log("I was clicked!")}>
-          <Text
-            style={{
-              backgroundColor: '#29ECCE',
-              fontSize: 0.07,
-              marginTop: 0.05,
-              layoutOrigin: [0.5, 0.5],
-              fontWeight: '400',
-              textAlign: 'center',
-              textAlignVertical: 'center',
-              transform: [{translate: [0, 0, -3]}],
-          }}>
-            Ain't no mountain high enough
-          </Text>
-        </VrButton>
-        <VrButton
-          style={{width: 1.0}}
-          onClick={()=> console.log("I was clicked!")}>
-          <Text
-            style={{
-              backgroundColor: '#29ECCE',
-              fontSize: 0.07,
-              marginTop: 0.05,
-              fontWeight: '400',
-              layoutOrigin: [0.5, 0.5],
-              textAlign: 'center',
-              textAlignVertical: 'center',
-              transform: [{translate: [0, 0, -3]}],
-          }}>
-            I want a baguette
-          </Text>
-        </VrButton>
+        </View>
+        <View>
+          <Button buttonClick={() => this.zenClicked(0)} text="I'm feeling beachy keen" />
+          <Button buttonClick={() => this.zenClicked(1)} text="Ain't no mountain high enough" />
+          <Button buttonClick={() => this.zenClicked(2)} text="I want a baguette" />
+        </View>
       </View>
     );
   }
---
+++ vr/consts/zens.js
@@ -0,0 +1,16 @@
+const zens = [
+    {   mantra: "Ohm",
+        image: "hawaii_beach.jpg"
+    },
+    {   mantra: "Ohm ",
+        image: "horseshoe_bend.jpg"
+    },
+    {   mantra: "Ohm Paris",
+        image: "sunrise_paris_2.jpg"
+    },
+    {   mantra: "",
+        image: "homebase.png"
+    }
+]
+
+export default zens;
